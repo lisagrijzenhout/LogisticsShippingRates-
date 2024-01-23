9835eef8a994713f9e404a0d46f9b9f38e9ca8b5
lisagrijzenhout <lisagrijzenhout@gmail.com>
2024-01-23 10:22:13 -0500

    added a new line to Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,5 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
- # Here is a test update by lisagrijzenhout
+ # Here is another test update by lisagrijzenhout
 
